0cdc18a22e085129e0e319259d63a1ec6a5faa3f
rachaelvphillips <rachaelvphillips@gmail.com>
2018-04-30 18:12:54 -0700

    data

--- Project4.Imaging/code/03_SuperLearner_RP.R
+++ Project4.Imaging/code/03_SuperLearner_RP.R
@@ -1,14 +1,14 @@
 library(SuperLearner)
 
+folds <- 10
 # set up Super Learner library
 lib <-
 
 # run cross-validated Super Learner? If possible
-cvSL <- CV.SuperLearner(Y=dat[,1], X=dat[,-1], newX= ,
+cvSL <- CV.SuperLearner(Y=dat[,28], X=dat[,-c(28,29)],
   method = "method.NNloglik", family = "binomial", SL.library = lib,
   parallel = 'multicore', cvControl = SuperLearner.CV.control(V = folds,
   stratifyCV = TRUE), innerCvControl = rep(list(SuperLearner.CV.control(V =
   folds, stratifyCV = TRUE)), folds))
 summary_cvSL <- summary.CV.SuperLearner(cvSL)
-rm(cvSL)
 cvSL_results <- summary_cvSL$Table
