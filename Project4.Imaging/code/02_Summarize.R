118340247bd1cbf6db681fa73d244c1ec0246113
rachaelvphillips <rachaelvphillips@gmail.com>
2018-04-30 18:04:27 -0700

    updates

--- Project4.Imaging/code/02_Summarize.R
+++ Project4.Imaging/code/02_Summarize.R
@@ -1,11 +1,11 @@
 library(dplyr)
 library(e1071)
 
-# make sure the Var and finalFreq_scaled are numeric
-dat$Var1 <- as.numeric(dat$Var1)
-dat$finalFreq_scaled <- as.numeric(dat$finalFreq_scaled)
-
-run_summ <- function(dat){
+load("~/Downloads/training_data-4.Rdata")
+df2 <- data.frame(t(training_data))
+df2$Var1 <- as.numeric(as.character(rownames(df2)))
+cp <- data.frame(df2[c(4072, 4071),])
+dat <- df2[-c(1, 4072, 4071),]
 
 HUsect <- ifelse(dat$Var1 <= -1000, "HU_A",
           ifelse(dat$Var1 > -1000 & dat$Var1 <= -900, "HU_B",
@@ -31,24 +31,106 @@ HUsect <- ifelse(dat$Var1 <= -1000, "HU_A",
           ifelse(dat$Var1 > 1000, "HU_V", NA))))))))))))))))))))))
 
 dat1 <- data.frame(dat, HUsect = as.factor(HUsect))
-dat_summ <- dat1 %>%
-  group_by(HUsect.2) %>%
-  summarise(Frequency = sum(finalFreq_scaled))
-colnames(dat_summ) <- c("covariate", "value")
+dat2 <- data.frame(dat1[,-1398])
 
-Kurtosis <- kurtosis(dat_summ$value)
-Variance <- var(dat_summ$value)
-Skew <- skewness(dat_summ$value)
-Mean <- mean(dat_summ$value)
-Median <- median(dat_summ$value)
-PatientID <- unique(dat$patientid)
-df <- t(data.table(Kurtosis, Variance, Skew, Mean, Median, PatientID))
+cols = seq(1:1397)    
+dat2[,cols] = apply(dat2[,cols], 2, function(x) as.numeric(as.character(x)))
+
+dat_summ <- dat2 %>%
+  dplyr::group_by(HUsect) %>%
+  dplyr::summarise_all(sum)
+
+
+Kurtosis <- apply(dat_summ[2:1398],2, kurtosis)  
+Variance <- apply(dat_summ[2:1398],2, var)
+Skew <- apply(dat_summ[2:1398],2, skewness)
+Mean <- apply(dat_summ[2:1398],2, mean)
+Median <- apply(dat_summ[2:1398],2, median)
+df <- t(data.frame(Kurtosis, Variance, Skew, Mean, Median))
 df1 <- data.frame(names = row.names(df), df)
-colnames(df1) <- c("covariate", "value")
 
-result <- t(rbind(dat_summ, df1))
-colnames(result) <- result[1,]
-result <- t(data.frame(result[-1,]))
+colnames(df1)[1] <- "covariate"
+colnames(dat_summ)[1] <- "covariate"
+  
+result <- rbind(dat_summ, df1)
 
-return(result)
-}
+cp1 <- data.frame(names = row.names(cp), cp)
+colnames(cp1)[1] <- "covariate"
+cp2 <- cp1[,-1399]
+
+result1 <- rbind(result, cp2)
+result2 <- data.frame(t(result1))
+train_summ <- result2
+colnames(train_summ) <- lapply(train_summ[1, ], as.character)
+train_summ <- train_summ[-1,]
+save(train_summ, file = "~/Desktop/train_summ.RData")
+
+##########################################################################################
+# test data
+##########################################################################################
+load("~/Downloads/test_data-2.Rdata")
+df6 <- data.frame(t(test_data))
+df6$Var1 <- as.numeric(as.character(rownames(df6)))
+cp6 <- data.frame(df6[c(4072, 1),])
+dat <- df6[-c(1, 4072, 4071),]
+
+HUsect <- ifelse(dat$Var1 <= -1000, "HU_A",
+          ifelse(dat$Var1 > -1000 & dat$Var1 <= -900, "HU_B",
+          ifelse(dat$Var1 > -900 & dat$Var1 <= -800, "HU_C",
+          ifelse(dat$Var1 > -800 & dat$Var1 < -700, "HU_D",
+          ifelse(dat$Var1 >= -700 & dat$Var1 <= -600, "HU_E",
+          ifelse(dat$Var1 > -600 & dat$Var1 <= -500, "HU_F",
+          ifelse(dat$Var1 > -500 & dat$Var1 <= -400, "HU_G",
+          ifelse(dat$Var1 > -400 & dat$Var1 <= -300, "HU_H",
+          ifelse(dat$Var1 > -300 & dat$Var1 <= -200, "HU_I",
+          ifelse(dat$Var1 > -200 & dat$Var1 <= -100, "HU_J",
+          ifelse(dat$Var1 > -100 & dat$Var1 <= 0, "HU_K",
+          ifelse(dat$Var1 > 0 & dat$Var1 <= 100, "HU_L",
+          ifelse(dat$Var1 > 100 & dat$Var1 <= 200, "HU_M",
+          ifelse(dat$Var1 > 200 & dat$Var1 <= 300, "HU_N",
+          ifelse(dat$Var1 > 300 & dat$Var1 <= 400, "HU_O",
+          ifelse(dat$Var1 > 400 & dat$Var1 <= 500, "HU_P",
+          ifelse(dat$Var1 > 500 & dat$Var1 <= 600, "HU_Q",
+          ifelse(dat$Var1 > 600 & dat$Var1 <= 700, "HU_R",
+          ifelse(dat$Var1 > 700 & dat$Var1 <= 800, "HU_S",
+          ifelse(dat$Var1 > 800 & dat$Var1 <= 900, "HU_T",
+          ifelse(dat$Var1 > 900 & dat$Var1 <= 1000, "HU_U",
+          ifelse(dat$Var1 > 1000, "HU_V", NA))))))))))))))))))))))
+
+dat1 <- data.frame(dat, HUsect = as.factor(HUsect))
+dat2 <- data.frame(dat1[,-199])
+
+cols = seq(1:198)    
+dat2[,cols] = apply(dat2[,cols], 2, function(x) as.numeric(as.character(x)))
+
+dat_summ <- dat2 %>%
+  dplyr::group_by(HUsect) %>%
+  dplyr::summarise_all(sum)
+
+
+Kurtosis <- apply(dat_summ[2:199],2, kurtosis)  
+Variance <- apply(dat_summ[2:199],2, var)
+Skew <- apply(dat_summ[2:199],2, skewness)
+Mean <- apply(dat_summ[2:199],2, mean)
+Median <- apply(dat_summ[2:199],2, median)
+df <- t(data.frame(Kurtosis, Variance, Skew, Mean, Median))
+df1 <- data.frame(names = row.names(df), df)
+
+colnames(df1)[1] <- "covariate"
+colnames(dat_summ)[1] <- "covariate"
+
+result <- rbind(dat_summ, df1)
+
+cp7 <- data.frame(names = row.names(cp6), cp6)
+colnames(cp7)[1] <- "covariate"
+cp2 <- cp7[,-200]
+
+result1 <- rbind(result, cp2)
+result2 <- data.frame(t(result1))
+colnames(result2) <- lapply(result2[1, ], as.character)
+result2 <- result2[-1,]
+test_summ <- result2
+save(test_summ, file = "~/Desktop/test_summ.RData")
+
+
+
