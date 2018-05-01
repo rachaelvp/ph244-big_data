6893063d1a3de59016f8ea8add2ecb8ed1565ef5
rachaelvphillips <rachaelvphillips@gmail.com>
2018-05-01 14:31:05 -0700

    updates

--- Project4.Imaging/code/03_SuperLearner_RP.R
+++ Project4.Imaging/code/03_SuperLearner_RP.R
@@ -2,12 +2,19 @@ library(SuperLearner)
 
 folds <- 10
 # set up Super Learner library
-lib <-
+lib <- list(c("SL.glm"),
+            c("SL.bayesglm"),
+            c("SL.mean"),
+            c("SL.qda"),
+            c("SL.gam"),
+            c("SL.svm"),
+            c("SL.ranger"),
+            c("SL.glmnet"))
 
 # run cross-validated Super Learner? If possible
 cvSL <- CV.SuperLearner(Y=dat[,28], X=dat[,-c(28,29)],
-  method = "method.NNloglik", family = "binomial", SL.library = lib,
-  parallel = 'multicore', cvControl = SuperLearner.CV.control(V = folds,
+  method = "method.AUC", family = "binomial", SL.library = lib,
+  cvControl = SuperLearner.CV.control(V = folds,
   stratifyCV = TRUE), innerCvControl = rep(list(SuperLearner.CV.control(V =
   folds, stratifyCV = TRUE)), folds))
 summary_cvSL <- summary.CV.SuperLearner(cvSL)
