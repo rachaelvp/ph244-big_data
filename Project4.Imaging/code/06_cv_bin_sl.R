0ffee7dcae9bcf2bc4170d102acde95d83d93ef3
tommyixi <Carpenito.t@gmail.com>
2018-05-01 22:05:59 -0700

    Adds code for binned CV super learner

--- Project4.Imaging/code/06_cv_bin_sl.R
+++ Project4.Imaging/code/06_cv_bin_sl.R
@@ -1,7 +1,12 @@
 library("SuperLearner")
+train_data <- read.csv2("train_summ.csv")
 
-train_data <- read.csv("/Users/Tommy/Desktop/ph244-big_data/Project4.Imaging/data/train_summ.csv")
 train_data <- train_data[complete.cases(train_data), ]
+
+drops <- c("Kurtosis","Skew")
+train_data <- train_data[ , !(names(train_data) %in% drops)]
+
+
 extra_covariates <- c("id", "cancer")
 training_data_x <- train_data[ , !(names(train_data) %in% extra_covariates)]
 train_data <- train_data$cancer
