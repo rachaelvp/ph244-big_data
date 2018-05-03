2580e22b85b1d56b4539b50a4cb07aa9df825c61
tommyixi <Carpenito.t@gmail.com>
2018-05-02 22:59:05 -0700

    Adds code for the SL graphing capablities

--- Project4.Imaging/code/07_cv_sl_bin.R
+++ Project4.Imaging/code/07_cv_sl_bin.R
@@ -1,5 +1,7 @@
 library("SuperLearner")
-train_data <- read.csv("train_summ.csv")
+library("ck37r")
+
+train_data <- read.csv("../data/train_summ.csv")
 extra_covariates <- c("cancer")
 training_data_x <- train_data[ , !(names(train_data) %in% extra_covariates)]
 training_data_y <- train_data$cancer
@@ -13,3 +15,18 @@ bin_sl <- CV.SuperLearner(Y=training_data_y, X=training_data_x, family = 'binomi
                                                                              stratifyCV = TRUE)), 10))
 
 save(bin_sl, file = "bin_sl.rData")
+
+
+ck37r::cvsl_plot_roc(bin_sl, Y = bin_sl$Y,
+                     title = "Cross Validated AUC for
+                     Prediction of Lung Cancer Detection",
+                     digits = 4)
+
+bin_sl_nnls <- CV.SuperLearner(Y=training_data_y, X=training_data_x, family = 'binomial',
+                          SL.library = library,
+                          method = "method.NNLS",
+                          cvControl = SuperLearner.CV.control(V = 10, stratifyCV = TRUE),
+                          innerCvControl = rep(list(SuperLearner.CV.control(V = 10,
+                                                                            stratifyCV = TRUE)), 10))
+
+save(bin_sl_nnls, file = "bin_sl_nnls.rData")
